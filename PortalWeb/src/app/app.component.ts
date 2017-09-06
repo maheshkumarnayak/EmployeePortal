baf7100ced99ba1283f1317d98b7d3cea9f6b2f7
Mahesh Kumar Nayak <mahs.nayak@gmail.com>
2017-09-07 03:09:30 +0530

    Portal is added

---
+++ PortalWeb/src/app/app.component.ts
@@ -0,0 +1,28 @@
+﻿//import { Component } from '@angular/core';
+
+//@Component({
+//  selector: 'app-root',
+//  templateUrl: './app.component.html',
+//  styleUrls: ['./app.component.css']
+//})
+//export class AppComponent {
+//  title = 'app';
+//}
+import { Component, OnInit } from '@angular/core';
+
+import { Http } from '@angular/http';
+@Component({
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit {
+    constructor(private _httpService: Http) { }
+    title: string = "SHANU";
+    apiValues: string[] = [];
+    ngOnInit() {
+        this._httpService.get('/api/values').subscribe(values => {
+            this.apiValues = values.json() as string[];
+        });
+    }
+}  
